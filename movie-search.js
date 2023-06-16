4a912c4c4cf10f80589a9f3a0b85215e04d186ff
lakshay gupta <guptalakshay104@gmail.com>
2023-06-16 16:40:24 +0530

    completing movie search app

---
+++ movie-search.js
@@ -0,0 +1,53 @@
+// most popular movies list
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+// searched movies
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+const movieBox = document.querySelector("#movie-box");
+const search = document.querySelector("#search");
+
+const getMovies = async (api) => {
+    const response  = await fetch(api);
+    const data = await response.json();
+    // console.log(data);
+    showMovies(data.results);
+}
+
+const showMovies = (data) => {
+    // empty the movie box then fill it again according 
+    movieBox.innerHTML = "";
+    data.forEach( item => {
+        // console.log(item);
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.innerHTML = `
+        <img src="${IMGPATH + item.poster_path}" alt="" />
+          <div class="overlay">
+            <div class="title">
+              <h2>${item.original_title}</h2>
+              <span> ${item.vote_average} </span>
+            </div>
+            <h3>Overview</h3>
+            <p>
+              ${item.overview}
+            </p>
+          </div>
+        `;
+        movieBox.appendChild(box);
+    });
+}
+
+search.addEventListener("keyup",function(e){
+    if(e.target.value != ""){   // show movies as per searching 
+        getMovies(SEARCHAPI + e.target.value);
+    }
+    else{
+        getMovies(APIURL);  // is search box is empty then show popular movies
+    }
+})
+
+
+getMovies(APIURL);  // initial call
+
